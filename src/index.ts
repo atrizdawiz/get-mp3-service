6bc2d54943fad1e61b681f6150e12003a5680b45
Andreas Östling <andreas.ostling@ur.se>
2023-02-05 19:17:04 +0100

    change port to 5000

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.use(cors());
 app.options("*", cors());
 app.use(express.json());
 
-const port = 3000;
+const port = 5000;
 
 app.get("/get-mp3/:youtubeId", async (req, res) => {
   const { youtubeId } = req.params;
